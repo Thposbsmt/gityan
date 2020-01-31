7f5b30cc4f67f8388b16f9cfc2a9b5324d435efc
Jandere <iutzhanov@bk.ru>
2020-01-31 21:21:28 +0600

    Create User.cs

---
+++ gityan/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gityan.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public void Hello()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
